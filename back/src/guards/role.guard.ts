38008f9d8909d03fd25e8ddf63e11fca1a925141
MaxiSalguero <salgueromaximiliano8@gmail.com>
2024-06-14 20:14:43 -0300

    Proteccion de rutas implementada

--- back/src/guards/role.guard.ts
+++ back/src/guards/role.guard.ts
@@ -1,11 +1,36 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'src/users/user.enum';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    const userRoles = user['https://huellasdesperanza.com/roles']
+
+    const hasRole = () => requiredRoles.some((role) => userRoles.includes(role));
+    const valid = user && userRoles && hasRole();
+
+    if (!valid) {
+      throw new ForbiddenException(
+        'No tiene permisos para acceder a esta ruta',
+      );
+    }
+
+    return valid;
   }
 }
