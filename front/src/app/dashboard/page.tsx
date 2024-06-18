cf49e2ae0a72c2fed515a1503e1f5f5e8f73e9e4
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-06-18 07:00:01 -0300

    se agrego dashboard de refugio

--- front/src/app/dashboard/page.tsx
+++ front/src/app/dashboard/page.tsx
@@ -1,27 +1,53 @@
+// 'use client';
+
+// import { useEffect, useState } from 'react';
+// import { checkUserRole } from '@/libs/auth';
+// // import SimulateLogin from '../SIMULATE_LOGIN/page';
+// import DashboardMain from '@/components/Dashboard/DashboardMain';
+// import withAuth from '@/HOC/WithAuth';
+
+
+
+// const DashboardPage: React.FC = () => {
+//   const [isAdmin, setIsAdmin] = useState(false);
+
+//   useEffect(() => {
+//     const role = checkUserRole();
+//     setIsAdmin(role === 'admin');
+//   }, []);
+
+//   return (
+//     <div className="flex h-screen">
+     
+//       <main className="flex-1 p-4 bg-gray-100 overflow-y-auto">
+//         {/* <SimulateLogin /> //*SIMULACION DE LOGIN "BORRAR"    */}
+//         <DashboardMain isAdmin={isAdmin} />
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default withAuth(DashboardPage);
+
 'use client';
 
 import { useEffect, useState } from 'react';
 import { checkUserRole } from '@/libs/auth';
-// import SimulateLogin from '../SIMULATE_LOGIN/page';
 import DashboardMain from '@/components/Dashboard/DashboardMain';
-import withAuth from '@/HOC/withAuth';
-
-
+import withAuth from '@/HOC/WithAuth';
 
 const DashboardPage: React.FC = () => {
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [role, setRole] = useState<'admin' | 'user' | 'shelter'>('user');
 
   useEffect(() => {
     const role = checkUserRole();
-    setIsAdmin(role === 'admin');
+    setRole(role);
   }, []);
 
   return (
     <div className="flex h-screen">
-     
       <main className="flex-1 p-4 bg-gray-100 overflow-y-auto">
-        {/* <SimulateLogin /> //*SIMULACION DE LOGIN "BORRAR"    */}
-        <DashboardMain isAdmin={isAdmin} />
+        <DashboardMain role={role} />
       </main>
     </div>
   );
