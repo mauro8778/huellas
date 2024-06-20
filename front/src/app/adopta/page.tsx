9ca1847e65f5e1a25274b6774859e87b47a505a8
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-06-19 21:40:02 -0400

    Filtros almacenados en el localStorage

--- front/src/app/adopta/page.tsx
+++ front/src/app/adopta/page.tsx
@@ -1,5 +1,110 @@
-'use client';
+// 'use client';
 
+
+// import React, { useState, useEffect, lazy, Suspense } from 'react';
+// import { IMascotas } from '@/interface/IMascotas';
+// import ModalFilterMascotas from '@/components/Card-Animals/FiltroMascotas/ModalFilterMascotas';
+// import withAuth from '@/HOC/withAuth';
+
+// const ListaMascotas = lazy(() => import('@/components/Card-Animals/ListaMascotas'));
+
+// const Adopta = () => {
+//   const [mascotasState, setMascotasState] = useState<IMascotas[]>([]);
+//   const [filterModalVisible, setFilterModalVisible] = useState<boolean>(false);
+//   const [filters, setFilters] = useState<{ edad: string; tamaño: string; sexo: string; especie: string }>({ edad: '', tamaño: '', sexo: '', especie: ''});
+//   const [filterOptions, setFilterOptions] = useState<{ edades: number[]; tamaños: string[]; sexos: string[], especies: string[] }>({ edades: [], tamaños: [], sexos:[], especies:[] });
+
+//   useEffect(() => {
+//     const fetchMascotas = async () => {
+//       try {
+//         const response = await fetch('https://huellasdesperanza.onrender.com/search/pets');
+//         if (!response.ok) {
+//           throw new Error('Error al obtener los datos de las mascotas!');
+//         }
+//         const data: IMascotas[] = await response.json();
+//         setMascotasState(data);
+//         const edades = Array.from(new Set(data.map(mascota => mascota.age || 0)));
+//         const tamaños = Array.from(new Set(data.map(mascota => mascota.pet_size || '')));
+//         const razas = Array.from(new Set(data.map(mascota => mascota.breed || '')));
+//         const sexos = Array.from(new Set(data.map(mascota => mascota.sexo || '')));
+//         const especies = Array.from(new Set(data.map(mascota => mascota.species || '')));
+//         setFilterOptions({ edades, tamaños, sexos, especies });
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchMascotas();
+//   }, []);
+
+//   const handleFilter = (edad: string, tamaño: string, sexo: string, especie: string) => {
+//     console.log('Aplicando filtros:', { edad, tamaño, sexo, especie });
+//     setFilters({ edad, tamaño, sexo, especie });
+//     setFilterModalVisible(false);
+//   };
+
+//   const filtrarMascotas = () => {
+//     return mascotasState.filter(mascota => {
+//       const edadCoincide = filters.edad ? (
+//         (filters.edad === 'cachorro' && mascota.age && mascota.age <= 1) ||
+//         (filters.edad === 'adulto' && mascota.age && mascota.age > 1 && mascota.age <= 5) ||
+//         (filters.edad === 'senior' && mascota.age && mascota.age > 5)
+//       ) : true;
+//       const tamañoCoincide = filters.tamaño ? mascota.pet_size === filters.tamaño : true;
+//       const sexoCoincide = filters.sexo ? mascota.sexo === filters.sexo : true;
+//       const especieCoincide = filters.especie ? mascota.species === filters.especie : true;
+//       return edadCoincide && tamañoCoincide && sexoCoincide && especieCoincide;
+//     });
+//   };
+
+//   const updateMascota = (updatedMascota: IMascotas) => {
+//     setMascotasState(prevState =>
+//       prevState.map(mascota =>
+//         mascota.id === updatedMascota.id ? updatedMascota : mascota
+//       )
+//     );
+//   };
+
+//   const deleteMascotaFromList = (mascotaId: string) => {
+//     setMascotasState(prevState => prevState.filter(m => m.id !== mascotaId));
+//   };
+
+//   const filteredMascotas = filtrarMascotas();
+
+//   return (
+//     <main className="flex flex-col items-center bg-gray-300">
+//       <div className="flex justify-center space-x-2">
+//         <button onClick={() => setFilterModalVisible(true)} className="mt-3 text-white bg-green-700 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+//           Filtrar Mascotas
+//         </button>
+//       </div>
+//       <Suspense fallback={<div>Cargando mascotas...</div>}>
+//         {filteredMascotas.length > 0 ? (
+//           <ListaMascotas 
+//             mascotas={filteredMascotas} 
+//             updateMascota={updateMascota}
+//             deleteMascota={deleteMascotaFromList}
+//           />
+//         ) : (
+//           <div>No se encontraron mascotas con los filtros seleccionados!</div>
+//         )}
+//       </Suspense>
+//       <ModalFilterMascotas
+//         isOpen={filterModalVisible}
+//         onClose={() => setFilterModalVisible(false)}
+//         onFilter={handleFilter}
+//         edades={filterOptions.edades}
+//         tamaños={filterOptions.tamaños}
+//         sexos={filterOptions.sexos}
+//         especies={filterOptions.especies}
+//       />
+//     </main>
+//   );
+// };
+
+// export default withAuth(Adopta);
+
+
+'use client';
 
 import React, { useState, useEffect, lazy, Suspense } from 'react';
 import { IMascotas } from '@/interface/IMascotas';
@@ -11,10 +116,15 @@ const ListaMascotas = lazy(() => import('@/components/Card-Animals/ListaMascotas
 const Adopta = () => {
   const [mascotasState, setMascotasState] = useState<IMascotas[]>([]);
   const [filterModalVisible, setFilterModalVisible] = useState<boolean>(false);
-  const [filters, setFilters] = useState<{ edad: string; tamaño: string; raza: string; sexo: string; especie: string }>({ edad: '', tamaño: '', raza: '', sexo: '', especie: ''});
-  const [filterOptions, setFilterOptions] = useState<{ edades: number[]; tamaños: string[]; razas: string[]; sexos: string[], especies: string[] }>({ edades: [], tamaños: [], razas: [], sexos:[], especies:[] });
+  const [filters, setFilters] = useState<{ edad: string; tamaño: string; sexo: string; especie: string }>({ edad: '', tamaño: '', sexo: '', especie: ''});
+  const [filterOptions, setFilterOptions] = useState<{ edades: number[]; tamaños: string[]; sexos: string[], especies: string[] }>({ edades: [], tamaños: [], sexos:[], especies:[] });
 
   useEffect(() => {
+    const savedFilters = localStorage.getItem('mascotasFilters');
+    if (savedFilters) {
+      setFilters(JSON.parse(savedFilters));
+    }
+
     const fetchMascotas = async () => {
       try {
         const response = await fetch('https://huellasdesperanza.onrender.com/search/pets');
@@ -28,7 +138,7 @@ const Adopta = () => {
         const razas = Array.from(new Set(data.map(mascota => mascota.breed || '')));
         const sexos = Array.from(new Set(data.map(mascota => mascota.sexo || '')));
         const especies = Array.from(new Set(data.map(mascota => mascota.species || '')));
-        setFilterOptions({ edades, tamaños, razas, sexos, especies });
+        setFilterOptions({ edades, tamaños, sexos, especies });
       } catch (error) {
         console.error(error);
       }
@@ -36,9 +146,11 @@ const Adopta = () => {
     fetchMascotas();
   }, []);
 
-  const handleFilter = (edad: string, tamaño: string, raza: string, sexo: string, especie: string) => {
-    console.log('Aplicando filtros:', { edad, tamaño, raza, sexo, especie });
-    setFilters({ edad, tamaño, raza, sexo, especie });
+  const handleFilter = (edad: string, tamaño: string, sexo: string, especie: string) => {
+    console.log('Aplicando filtros:', { edad, tamaño, sexo, especie });
+    const filters = { edad, tamaño, sexo, especie };
+    setFilters(filters);
+    localStorage.setItem('mascotasFilters', JSON.stringify(filters));
     setFilterModalVisible(false);
   };
 
@@ -50,10 +162,9 @@ const Adopta = () => {
         (filters.edad === 'senior' && mascota.age && mascota.age > 5)
       ) : true;
       const tamañoCoincide = filters.tamaño ? mascota.pet_size === filters.tamaño : true;
-      const razaCoincide = filters.raza ? mascota.breed === filters.raza : true;
       const sexoCoincide = filters.sexo ? mascota.sexo === filters.sexo : true;
       const especieCoincide = filters.especie ? mascota.species === filters.especie : true;
-      return edadCoincide && tamañoCoincide && razaCoincide && sexoCoincide && especieCoincide;
+      return edadCoincide && tamañoCoincide && sexoCoincide && especieCoincide;
     });
   };
 
@@ -95,7 +206,6 @@ const Adopta = () => {
         onFilter={handleFilter}
         edades={filterOptions.edades}
         tamaños={filterOptions.tamaños}
-        razas={filterOptions.razas}
         sexos={filterOptions.sexos}
         especies={filterOptions.especies}
       />
--- front/src/components/Card-Animals/FiltroMascotas/ModalFilterMascotas.tsx
+++ front/src/components/Card-Animals/FiltroMascotas/ModalFilterMascotas.tsx
@@ -1,46 +1,99 @@
+// import React, { useState } from 'react';
+// import FiltroEdad from './FiltroEdad';
+// import FiltroTamaño from './FiltroTamaño';
+// import FiltroEspecie from './FiltroEspecie';
+// import FiltroSexo from './FiltroSexo';
+
+// interface ModalProps {
+//   onClose: () => void;
+//   onFilter: (edad: string, tamaño: string, sexo: string, especie: string) => void;
+//   isOpen: boolean;
+//   edades: number[];
+//   tamaños: string[];
+//   especies: string[];
+//   sexos: string[]
+// }
+
+// const ModalFilterMascotas: React.FC<ModalProps> = ({ onClose, onFilter, isOpen, edades, tamaños, especies, sexos }) => {
+//   const [edad, setEdad] = useState('');
+//   const [tamaño, setTamaño] = useState('');
+//   const [sexo, setSexo] = useState('');
+//   const [especie, setEspecie] = useState('');
+
+//   const handleFilter = () => {
+//     console.log('Filtros aplicados:', { edad, tamaño, sexo, especie });
+//     onFilter(edad, tamaño, sexo, especie);
+//     onClose();
+//   };
+
+//   return (
+//     <div className={`overflow-y-scroll fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+//       <div className={`bg-white p-6 rounded-lg shadow-lg transform transition-transform duration-300 ${isOpen ? 'scale-100' : 'scale-90'}`}>
+//         <h2 className="text-2xl font-bold mb-4">Filtrar Mascota</h2>
+//         <FiltroEdad edad={edad} setEdad={setEdad} edades={edades} />
+//         <FiltroSexo sexo={sexo} setSexo={setSexo} sexos={sexos} />
+//         <FiltroEspecie especie={especie} setEspecie={setEspecie} especies={especies} />
+//         <FiltroTamaño tamaño={tamaño} setTamaño={setTamaño} tamaños={tamaños} />
+//         <div className="flex justify-end space-x-2">
+//           <button onClick={handleFilter} className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-600">Aplicar Filtro</button>
+//           <button onClick={onClose} className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">Cerrar</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ModalFilterMascotas;
 import React, { useState } from 'react';
 import FiltroEdad from './FiltroEdad';
-import FiltroRaza from './FiltroRaza';
 import FiltroTamaño from './FiltroTamaño';
 import FiltroEspecie from './FiltroEspecie';
 import FiltroSexo from './FiltroSexo';
 
 interface ModalProps {
   onClose: () => void;
-  onFilter: (edad: string, tamaño: string, raza: string, sexo: string, especie: string) => void;
+  onFilter: (edad: string, tamaño: string, sexo: string, especie: string) => void;
   isOpen: boolean;
   edades: number[];
-  razas: string[];
   tamaños: string[];
   especies: string[];
   sexos: string[]
 }
 
-const ModalFilterMascotas: React.FC<ModalProps> = ({ onClose, onFilter, isOpen, edades, razas, tamaños, especies, sexos }) => {
+const ModalFilterMascotas: React.FC<ModalProps> = ({ onClose, onFilter, isOpen, edades, tamaños, especies, sexos }) => {
   const [edad, setEdad] = useState('');
-  const [raza, setRaza] = useState('');
   const [tamaño, setTamaño] = useState('');
   const [sexo, setSexo] = useState('');
   const [especie, setEspecie] = useState('');
 
   const handleFilter = () => {
-    console.log('Filtros aplicados:', { edad, tamaño, raza, sexo, especie });
-    onFilter(edad, tamaño, raza, sexo, especie);
+    console.log('Filtros aplicados:', { edad, tamaño, sexo, especie });
+    onFilter(edad, tamaño, sexo, especie);
     onClose();
+  };
+
+  const handleClear = () => {
+    setEdad('');
+    setTamaño('');
+    setSexo('');
+    setEspecie('');
+    localStorage.removeItem('mascotasFilters');
   };
 
   return (
     <div className={`overflow-y-scroll fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
       <div className={`bg-white p-6 rounded-lg shadow-lg transform transition-transform duration-300 ${isOpen ? 'scale-100' : 'scale-90'}`}>
+        <button onClick={onClose} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
+          &#x2715;
+        </button>
         <h2 className="text-2xl font-bold mb-4">Filtrar Mascota</h2>
         <FiltroEdad edad={edad} setEdad={setEdad} edades={edades} />
         <FiltroSexo sexo={sexo} setSexo={setSexo} sexos={sexos} />
         <FiltroEspecie especie={especie} setEspecie={setEspecie} especies={especies} />
         <FiltroTamaño tamaño={tamaño} setTamaño={setTamaño} tamaños={tamaños} />
-        <FiltroRaza raza={raza} setRaza={setRaza} razas={razas} />
         <div className="flex justify-end space-x-2">
           <button onClick={handleFilter} className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-600">Aplicar Filtro</button>
-          <button onClick={onClose} className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">Cerrar</button>
+          <button onClick={handleClear} className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600">Limpiar</button>
         </div>
       </div>
     </div>
