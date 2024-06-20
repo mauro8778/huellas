739f293efc49871b38acb4bbca2ea960f4cd2a28
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-06-20 09:59:13 -0400

    asd

--- front/src/app/dashboard/postulaciones/page.tsx
+++ front/src/app/dashboard/postulaciones/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import ToggleActiveShelter from '../../../components/admin/ToggleActiveShelter';
 import Swal from 'sweetalert2';
-import decodeJwt from '@/utils/decodeJwt';
+import { decodeJwt } from '@/utils/decodeJwt';
 
 const defaultAvatarUrl = '/shelter.webp';
 
