abc89be7bf4609b4b71e040bc79e4e96e0efff54
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2024-06-20 10:00:10 -0300

    stilos cards

--- front/src/components/NavBar/NavMenu.tsx
+++ front/src/components/NavBar/NavMenu.tsx
@@ -1,180 +1,4 @@
-// // import Link from 'next/link';
-// // import { useState } from 'react';
-// // import { MdMenu, MdClose } from 'react-icons/md'; 
 
-// // const NavMenu: React.FC = () => {
-// //   const [menuOpen, setMenuOpen] = useState(false);
-
-// //   const toggleMenu = () => {
-// //     setMenuOpen(!menuOpen);
-// //   };
-
-// //   return (
-// //     <nav className="relative">
-// //       <div className="flex items-center justify-between p-4">
-// //         <div className="text-2xl font-bold text-gray-100"></div>
-// //         <button
-// //           className="text-gray-100 md:hidden"
-// //           onClick={toggleMenu}
-// //           aria-label="Toggle menu"
-// //         >
-// //           {menuOpen ? <MdClose size={24} /> : <MdMenu size={24} />}
-// //         </button>
-// //       </div>
-// //       <ul className={`flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-10 text-gray-100 text-xl p-4 md:p-0 ${menuOpen ? 'block' : 'hidden md:flex'}`}>
-// //         <li>
-// //           <Link
-// //             className="hover:text-pink-300"
-// //             href={{
-// //               pathname: "/Home",
-// //               query: { name: "test" },
-// //             }}
-// //           >
-// //             Home
-// //           </Link>
-// //         </li>
-// //         <li>
-// //           <Link
-// //             className="hover:text-pink-300"
-// //             href={{
-// //               pathname: "/adopta",
-// //               query: { name: "test" },
-// //             }}
-// //           >
-// //             Adoptar
-// //           </Link>
-// //         </li>
-// //         <li>
-
-
-// //           <Link
-// //             className="hover:text-pink-300 hover:transform hover:scale-105 transition-transform duration-200"
-// //             href={{
-// //               pathname: "/refugios",
-// //               query: { name: "test" },
-// //             }}
-// //           >
-// //             Refugios
-// //           </Link>
-          
-// //         </li>
-// //         <li>
-// //           <Link className='hover:text-gray-600' href={{
-// //             pathname: "/comunidad",
-// //             query: {name: "test"},
-// //           }}>
-// //             Comunidad
-// //           </Link>
-// //         </li> 
-// //       </ul>
-// //     </nav>
-// //   );
-// // };
-
-// // export default NavMenu;
-
-
-
-
-
-// import Link from 'next/link';
-// import { useState, useEffect } from 'react';
-// import { MdMenu, MdClose } from 'react-icons/md';
-// import decodeJwt from '@/utils/decodeJwt';
-
-// const NavMenu: React.FC = () => {
-//   const [menuOpen, setMenuOpen] = useState(false);
-//   const [userRole, setUserRole] = useState('');
-
-//   const toggleMenu = () => {
-//     setMenuOpen(!menuOpen);
-//   };
-
-//   useEffect(() => {
-//     const userSessionStr = localStorage.getItem('userSession');
-    
-//     if (userSessionStr) {
-//       const userSession = JSON.parse(userSessionStr);
-      
-//       if (userSession && userSession.access_token) {
-//         const token = userSession.access_token;
-
-//         try {
-//           const decodedToken = decodeJwt(token);
-//           console.log(decodedToken);
-          
-
-//           setUserRole(decodedToken.role);
-          
-//         } catch (error) {
-//           console.error('Error decoding token', error);
-//         }
-//       }
-//     }
-//   }, []);
-
-//   return (
-//     <nav className="relative">
-//       <div className="flex items-center justify-between p-4">
-//         <div className="text-2xl font-bold text-gray-100"></div>
-//         <button
-//           className="text-gray-100 md:hidden"
-//           onClick={toggleMenu}
-//           aria-label="Toggle menu"
-//         >
-//           {menuOpen ? <MdClose size={24} /> : <MdMenu size={24} />}
-//         </button>
-//       </div>
-//       <ul className={`flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-10 text-gray-100 text-xl p-4 md:p-0 ${menuOpen ? 'block' : 'hidden md:flex'}`}>
-//         <li>
-//           <Link
-//             className="hover:text-pink-300"
-//             href={{
-//               pathname: "/Home",
-//               query: { name: "test" },
-//             }}
-//           >
-//             Home
-//           </Link>
-//         </li>
-//         {userRole !== 'Shelter' && (
-//           <li>
-//             <Link
-//               className="hover:text-pink-300"
-//               href={{
-//                 pathname: "/adopta",
-//                 query: { name: "test" },
-//               }}
-//             >
-//               Adoptar
-//             </Link>
-//           </li>
-//         )}
-//         <li>
-//           <Link
-//             className="hover:text-pink-300 hover:transform hover:scale-105 transition-transform duration-200"
-//             href={{
-//               pathname: "/refugios",
-//               query: { name: "test" },
-//             }}
-//           >
-//             Refugios
-//           </Link>
-//         </li>
-//         <li>
-//           <Link className='hover:text-gray-600' href={{
-//             pathname: "/comunidad",
-//             query: {name: "test"},
-//           }}>
-//             Comunidad
-//           </Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default NavMenu;
 
 
 import Link from 'next/link';
@@ -232,7 +56,7 @@ const NavMenu: React.FC = () => {
         {userRole !== 'Shelter' && (
 
           <Link
-            className="hover:text-pink-300"
+            className="hover:text--300"
             href={{
               pathname: "/Home",
               query: { name: "test" },
@@ -245,7 +69,7 @@ const NavMenu: React.FC = () => {
         {userRole !== 'Shelter' && (
           <li>
             <Link
-              className="hover:text-pink-300"
+              className="hover:text-gray-300"
               href={{
                 pathname: "/adopta",
                 query: { name: "test" },
@@ -260,7 +84,7 @@ const NavMenu: React.FC = () => {
         {userRole !== 'Shelter' && (
 
           <Link
-            className="hover:text-pink-300 hover:transform hover:scale-105 transition-transform duration-200"
+            className="hover:text-gray-300 hover:transform hover:scale-105 transition-transform duration-200"
             href={{
               pathname: "/refugios",
               query: { name: "test" },
@@ -271,7 +95,7 @@ const NavMenu: React.FC = () => {
 )}
         </li>
         <li>
-          <Link className='hover:text-gray-600' href={{
+          <Link className='hover:text-gray-300' href={{
             pathname: "/comunidad",
             query: {name: "test"},
           }}>
--- front/src/components/Refugios/CardRefuge.tsx
+++ front/src/components/Refugios/CardRefuge.tsx
@@ -11,7 +11,7 @@ const CardRefuge: React.FC<{ refugio: IRefugios }> = ({ refugio }) => {
   const truncatedTitle = refugio.shelter_name ? truncateDescription(refugio.shelter_name, 25) : '';
   const truncatedDescription = refugio.description ? truncateDescription(refugio.description, 55) : '';
   return (
-    <div className="bg-transparent rounded-lg shadow-2xl p-4 m-2 md:m-4 max-w-xs mx-auto transform transition-transform duration-200 hover:scale-105">
+    <div className="bg-transparent border-t-4 border-lime500 rounded-xl shadow-xl p-4 m-2 md:m-4 max-w-xs mx-auto transform transition-transform duration-200 hover:scale-105">
       <div className="h-64 relative overflow-hidden rounded-t-lg">
         <Image src={refugio.imgUrl} alt={refugio.name} layout="fill" objectFit="cover" />
       </div>
@@ -19,7 +19,7 @@ const CardRefuge: React.FC<{ refugio: IRefugios }> = ({ refugio }) => {
         <h5 className="text-xl font-semibold text-gray-900 dark:text-white">{truncatedTitle}</h5>
         <p className="mt-2 text-gray-700 dark:text-gray-400">{refugio.location} - {refugio.zona}</p>
         <p className="mt-2 text-gray-700 dark:text-gray-400">{truncatedDescription}</p>
-        <a href={`/refugios/${refugio.id}`} className="border border-gray-500 inline-block mt-4 px-4 py-2 text-sm font-semibold text-black rounded-lg hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
+        <a href={`/refugios/${refugio.id}`} className="border border-lime500 inline-block mt-4 px-4 py-2 text-sm font-semibold text-gray-800 rounded-xl hover:bg-lime500 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
           Leer más
         </a>
       </div>
